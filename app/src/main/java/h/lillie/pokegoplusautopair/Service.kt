911afc690134791333fa73439a5b3789671c640e
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-02-16 11:47:06 -0500

    Cleanup if checks

--- app/src/main/java/h/lillie/pokegoplusautopair/Service.kt
+++ app/src/main/java/h/lillie/pokegoplusautopair/Service.kt
@@ -7,13 +7,9 @@ import android.view.accessibility.AccessibilityNodeInfo
 
 class Service: AccessibilityService() {
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        if (event == null) return
+        if (event == null || event.packageName == null || event.source == null || event.contentChangeTypes != AccessibilityEvent.CONTENT_CHANGE_TYPE_UNDEFINED || event.className == null || event.className == "com.android.settings.bluetooth.BluetoothPairingDialog") return
 
-        if (event.contentChangeTypes != AccessibilityEvent.CONTENT_CHANGE_TYPE_UNDEFINED || event.packageName == null || event.className == null) return
-
-        if (event.packageName == "com.android.settings" && event.className == "com.android.settings.bluetooth.BluetoothPairingDialog") return
-
-        if (event.source != null && event.packageName == "com.android.settings" && Regex("[^A-Za-z0-9 ]").replace(event.text.toString(), "").contains("pair with pokemon go plus", true)) {
+        if (event.packageName == "com.android.settings" && Regex("[^A-Za-z0-9 ]").replace(event.text.toString(), "").contains("pair with pokemon go plus", true)) {
 
             // Android 13-15
             if (Build.VERSION.SDK_INT <= 35) {
