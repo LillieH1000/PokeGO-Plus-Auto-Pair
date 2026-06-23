1489bb6826cf540d76ed1345f8a17903303066ed
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-06-23 15:25:00 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "h.lillie.pokegoplusautopair"
         minSdk = 33
         targetSdk = 37
-        versionCode = 6
+        versionCode = 7
         versionName = "1.1.0"
     }
 
